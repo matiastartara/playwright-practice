b1403cc680f54edb369f8676df4dbfa017488b18
Matias <Matias@LAPTOP-SOREGMCP>
2023-07-30 15:53:45 -0300

    add invalid login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,35 @@
 import { test, expect } from '@playwright/test';
 import HomePage from '../pages/HomePage';
+import { LoginPage } from '../pages/LoginPage';
 
 const URL = 'https://magento.softwaretestingboard.com/';
-let home : HomePage;
-
+let home: HomePage;
+let login: LoginPage;
 
 test.describe('Login tests', () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto(URL);
         home = new HomePage(page);
-      });
-      
-      test.afterEach(async ({page}) => {
-          await page.close();
-      });
-      
-    test('Invalid login test', async({page}) => {
+        login = new LoginPage(page);
+    });
 
+    test.afterEach(async ({ page }) => {
+        await page.close();
+    });
+
+    test('Invalid login test', async ({ page }) => {
         await expect(page).toHaveTitle('Home Page');
         await home.clickOnSignIn();
         await expect(page).toHaveTitle('Customer Login');
+        await login.completeUsername('user@invalid.com');
+        await login.completePassword('12345');
+        await login.clickOnSignIn();
 
-        // let text = await page.locator('div header').textContent();
-
-
+        let errorMsg = await page.locator('.messages .error.message > div').textContent();
+        await expect(errorMsg).toEqual('Incorrect CAPTCHA');
+        const result = await login.isNotLoggedIn();
+        await expect(result).toBe(true);
     })
-
-
 
 });
